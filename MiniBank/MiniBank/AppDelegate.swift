ad5f50fa72d2bcc3d57ff12fdd7b21dbc65464e7
Noah <celestiarune@icloud.com>
2024-06-01 20:45:48 +0900

    Initialize UIWindow and set rootViewController

---
+++ MiniBank/MiniBank/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  MiniBank
+//
+//  Created by Ah Roh on 6/1/24.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = .orange
+        window?.rootViewController = ViewController()
+        
+        return true
+    }
+}
+
